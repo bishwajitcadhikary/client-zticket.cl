a419f7f2059279a70e5a69d774bb3382b9510807
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2024-07-09 02:39:59 +0600

    bug fixes

--- config/bundles.php
+++ config/bundles.php
@@ -43,4 +43,5 @@ return [
     Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle::class => ['all' => true],
     CoreSphere\ConsoleBundle\CoreSphereConsoleBundle::class => ['all' => true],
     Presta\SitemapBundle\PrestaSitemapBundle::class => ['all' => true],
+    Nelmio\SecurityBundle\NelmioSecurityBundle::class => ['all' => true],
 ];
